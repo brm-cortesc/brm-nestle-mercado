3c637b34f46888d0f879217789f7fa464c2622f5
Cristian Cortés <cristian.cortes@brm.com.co>
2018-02-13 15:30:16 -0500

    agregada funcionalidad para videos en galerias

--- publication/js/galeria-single.cdb.js
+++ publication/js/galeria-single.cdb.js
@@ -1,7 +1,23 @@
 jQuery(document).ready(function($) {
 
-	/*Animacion del layout de imagenes*/
-	var item = $('.galeria-item');
+
+	var showLayout = function (el) {
+		
+		var base = (el == '#videos')?'.item-video':'.item-img';
+		var item = jQuery(el + ' '+base); 
+
+		// console.log(base)
+		// console.log(item)
+		
+		if(el == '#videos' ){
+			jQuery('#imgs').addClass('hidden');
+		}else{
+			jQuery('#videos').addClass('hidden');
+		}
+
+		jQuery(el).removeClass('hidden');
+
+		jQuery(item).removeClass('fadeInUp bounce');
 		jQuery(item).addClass('animated');
 
 		/*Delay de animacion entre imagenes existentes*/
@@ -17,7 +33,61 @@ jQuery(document).ready(function($) {
 			byPass(x);
 		};
 
+		// console.log('ando')
 
+
+	};
+
+	
+	/*click para mostrar cada galeria*/
+	jQuery('.btn.btn-primary').click(function (e) {
+
+		e.preventDefault();
+		var pos = jQuery(window).scrollTop(),
+			loc = jQuery(this).attr('href');
+
+		//*Devolvemos a la url el ancla*//	
+		// window.location.hash = loc;
+
+		/*Evitamos el salto de la ventana al cambiar de hash*/
+		jQuery(window).scrollTop(pos)
+
+
+		jQuery('.btn.btn-primary').removeClass('active');
+
+		if(jQuery(this).hasClass('btn-img')){
+			
+			jQuery(this).addClass('active');
+			showLayout('#imgs')	
+			// console.log('img ')
+
+		}
+		else if(jQuery(this).hasClass('btn-video')){
+
+			jQuery(this).addClass('active')			
+			showLayout('#videos')	
+			// console.log('video ')
+
+
+		}
+
+	});
+
+
+	/*Validamos URL para mostrar galeria*/
+	var galActual = window.location.hash;
+
+	/*Evitamos scroll*/
+	var noScroll = function () {
+		setTimeout(function() {
+			if(  jQuery(window).scrollTop() > 0 ){
+				jQuery(window).scrollTop(0)
+			}
+
+		}, 200);
+	};
+
+	showLayout('#imgs');
 
 	//Se genera configuracion por cada galeria que se crea
 	jQuery('.galeria-single').each(function() {
@@ -34,6 +104,17 @@ jQuery(document).ready(function($) {
 			});
 		
 	});
+
+	jQuery('.galeria-video').each(function() {
+
+		jQuery(this).magnificPopup({
+			  type:'iframe',
+			  delegate:'.glyphicon-play',
+			  gallery:{enabled:true}
+			});
+		
+	});
+
 
 
 
